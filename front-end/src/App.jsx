9e9c6ebaf78d392bab7d1327acba8c64d1e5323a
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-03-29 18:22:18 +0000

    front-end configured and tested

---
+++ front-end/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [athletes, setAthletes] = useState([]);
+  const [loadingState, setLoadingState] = useState(false);
+
+  const getAthletes = async () => {
+    const response = await fetch("http://localhost:3000/");
+    const data = await response.json();
+    setAthletes(data.payload);
+  };
+
+  useEffect(() => {
+    if (athletes.length === 0) {
+      getAthletes();
+    }
+    return () => {};
+  }, [athletes]);
+
+  console.log(athletes);
+
+  return (
+    <>
+      <h1>StravaJS2.0</h1>
+      <p>{athletes.length >= 1 ? `${athletes[0].name}` : "Loading..."}</p>
+      {/* <p>{athletes[0].id}</p> */}
+    </>
+  );
+}
+
+export default App;
