cedf138360faac65a690ec4f8cc42a0a3e79584e
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-03-29 16:06:57 +0000

    .get '/' added successfully

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import { getAthletes } from "./models/athletes.js";
 
@@ -7,9 +8,13 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 
-const athletes = await getAthletes();
-console.log(athletes);
+app.get("/", async (req, res) => {
+  // use func to get all athletes
+  const athletes = await getAthletes();
+  res.status(200).json({ status: "success", payload: athletes });
+});
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
