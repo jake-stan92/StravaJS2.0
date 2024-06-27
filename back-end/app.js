4c51ea0b6f33ad49e38b25141426361035937ee7
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-06-27 16:16:53 +0100

    cors reverted, missed on last commit

--- back-end/app.js
+++ back-end/app.js
@@ -15,14 +15,14 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-// app.use(cors({ origin: "*" }));
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+app.use(cors({ origin: "*" }));
+// app.use(function (req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 // global date data here
 const todaysDate = new Date();
