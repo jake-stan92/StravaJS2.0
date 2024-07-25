4fe9abb98d0002045851b038fdbcc820e49367c6
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-07-25 15:48:56 +0100

    a tag added to header

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -8,6 +8,12 @@ const Header = (props) => {
       <p>
         Viewing data for {props.athlete.firstName} {props.athlete.lastName}
       </p>
+      <p>
+        Want more?{" "}
+        <a href="https://www.strava.com/athlete/training" target="_blank">
+          View On Strava
+        </a>
+      </p>
     </div>
   );
 };
